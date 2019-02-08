73f8c6ae2ce68af74fd9a555d14864bd2fc8ca0a
mmiah00 <mmiah00@stuty.edu>
2019-02-08 14:52:10 -0500

    driver:

--- Driver.java
+++ Driver.java
@@ -1,6 +1,20 @@
 public class Driver {
   public static void main (String [] args) {
-    QueenBoard testing = new QueenBoard (2);
+    QueenBoard testing = new QueenBoard (8);
+    System.out.println (testing.toString ());
+    System.out.println (testing.addQueen (3,4));
+    System.out.println (testing.addQueen (6,2));
+    System.out.println (testing.addQueen (4,0));
+    System.out.println (testing.addQueen (1,6));
+    System.out.println (testing.toString());
+    System.out.println (testing.removeQueen (6,2));
+    System.out.println (testing.removeQueen (4,0));
+    System.out.println (testing.toString ());
+    System.out.println (testing.removeQueen (0,0));
+    System.out.println (testing.removeQueen (-1, 10));
+    System.out.println (testing.removeQueen (-3, 22));
+    System.out.println (testing.addQueen (200,2));
+    System.out.println (testing.addQueen (-3,2));
     System.out.println (testing.toString ());
   }
 }
